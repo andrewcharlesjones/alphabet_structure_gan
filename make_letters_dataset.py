484f61dabd6103f4b7565cd25b4470b2f5722495
Andrew Jones <ajones788@gmail.com>
2017-08-09 10:42:47 -0400

    fixed dataset

--- make_letters_dataset.py
+++ make_letters_dataset.py
@@ -6,13 +6,15 @@ import time
 from scipy.misc import imsave
 import os
 from scipy.misc import imread
+from scipy.misc import imresize
 
-X_LOC = [3, 48]
-Y_LOC = [1425, 1470]
+X_LOC = [146, 212]
+Y_LOC = [1232, 1308]
 
 # make sure the terminal where this is printing is open when you call this
 def screenshot_screen(letter):
-	print letter
+	print '    ' + letter
+	print '\n'
 	time.sleep(0.5)
 	im = ImageGrab.grab()
 
@@ -29,11 +31,24 @@ def save_letters():
 def crop_letters(letter_dir):
 	letter_imgs = os.listdir(letter_dir)
 	for im_fname in letter_imgs:
+		if 'cropped' in im_fname or '.DS' in im_fname:
+			continue
 		im = imread(os.path.join(letter_dir, im_fname))
+		
+
 		cropped_im = im[Y_LOC[0]:Y_LOC[1], X_LOC[0]:X_LOC[1]]
+		# plt.imshow(cropped_im)
+		# plt.show()
+		cropped_im = imresize(cropped_im, [28, 28])
 		imsave(os.path.join(letter_dir, im_fname.split('.')[0] + '_cropped.png'), cropped_im)
+
+def remove_full_screenshots(data_dir):
+	files = os.listdir(data_dir)
+	for f in files:
+		if 'cropped' not in f and '.DS' not in f:
+			os.remove(os.path.join(data_dir, f))
 
 if __name__ == '__main__':
 	# save_letters()
 	crop_letters('data')
-
+	remove_full_screenshots('data')
